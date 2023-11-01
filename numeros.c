0ba167f4f235df0dd8158bd8578e4176644c03ad
Martin Burga <martin.burga@udla.edu.ec>
2023-11-01 16:27:50 -0500

    Calculo de posicion de los numeros que ha insertado el usuario.

--- numeros.c
+++ numeros.c
@@ -1,10 +1,16 @@
+// Realice un programa que lea un número entero positivo n y a continuación lea n números y despliegue tanto el valor
+// máximo como la posición en el conjunto de números introducido donde ocurre el máximo.
+// A su vez, también realice lo mismo pero para el mínimo valor.
+
 #include <stdio.h>
 
 int numero;
 int nummax=0;
-int nummin=1;
+int nummin=10000010;
 int cant;
 int opcion;
+int jmax;
+int jmin;
 
 int main(){
     printf("Bievenido a la calculadora de valores maximos y minimos.\n");
@@ -25,15 +31,20 @@ int main(){
                 {
                     nummax=0;
                     nummax=nummax+numero;
+                    jmax=i;
                 }
-                if (numero<nummin)
+                if (numero<=nummin)
                 {
                     nummin=0;
                     nummin=nummin+numero;
+                    jmin=i;
                 }
             }
             printf("El numero maximo que has insertado es el: %d\n", nummax);
+            printf("Esta en la posicion %d de todos los numeros que insertaste.\n", jmax);
             printf("El numero minimo que has insertado es el: %d\n", nummin);
+            printf("Esta en la posicion %d de todos los numeros que insertaste.\n", jmin);
+            printf("||------------------------------------------------||\n");
 
         }
         
