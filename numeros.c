7741b3585afc7b13c1c456fcd5389c011ffb219e
Martin Burga <martin.burga@udla.edu.ec>
2023-11-01 15:52:32 -0500

    Estructura repetitiva FOR. Esta actua como un pequeño contador e incrementara su cantidad en 1 cada vez que se inserta y se lea un numero.

--- numeros.c
+++ numeros.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 
 int numero;
+int cant;
 int opcion;
 
 int main(){
-    printf("Bievenido a la calculadora de numeros mayores.\n");
+    printf("Bievenido a la calculadora de valores maximos y minimos.\n");
     do {
+        printf("||------------------------------------------------||\n");
         printf("Inserta 1 para empezar.\n");
         printf("Inserta 2 para salir.\n");
         scanf("%d", &opcion);
+        if (opcion==1)
+        {
+            printf("Inserte la cantidad de numeros que desee ingresar.\n");
+            scanf("%d", &cant);
+            for (int i = 0; i <= cant; i++)
+            {
+                printf("Ingrese un numero.\n");
+                scanf("%d", &numero);
+
+            }
+            
+
+        }
+        
     } while(opcion==1);
     return 0;
 }
